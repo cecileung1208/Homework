fe87336cc6358ed9fd30aa2687d2d70901fefff9
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2020-11-24 15:45:46 -0500

    update

--- Unit 7 - SQL Challenge/Employee SQL/schema.sql
+++ Unit 7 - SQL Challenge/Employee SQL/schema.sql
@@ -15,25 +15,19 @@ CREATE TABLE "Departments" (
 );
 
 CREATE TABLE "Dept_Employees" (
-    "dept_no" varchar   NOT NULL,
     "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_Dept_Employees" PRIMARY KEY (
-        "emp_no"
-     )
+    "dept_no" varchar   NOT NULL
 );
 
 CREATE TABLE "Dept_Manager" (
-    "dept_no" int   NOT NULL,
-    "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_Dept_Manager" PRIMARY KEY (
-        "emp_no"
-     )
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL
 );
 
 CREATE TABLE "Employees" (
     "emp_no" int   NOT NULL,
     "emp_title_id" varchar   NOT NULL,
-    "birth_date" dater   NOT NULL,
+    "birth_date" date   NOT NULL,
     "first_name" varchar   NOT NULL,
     "last_name" varchar   NOT NULL,
     "sex" char(1)   NOT NULL,
@@ -44,10 +38,10 @@ CREATE TABLE "Employees" (
 );
 
 CREATE TABLE "Salaries" (
-    "emp-no" int   NOT NULL,
+    "emp_no" int   NOT NULL,
     "salary" float   NOT NULL,
     CONSTRAINT "pk_Salaries" PRIMARY KEY (
-        "emp-no"
+        "emp_no"
      )
 );
 
@@ -59,11 +53,11 @@ CREATE TABLE "Titles" (
      )
 );
 
-ALTER TABLE "Dept_Employees" ADD CONSTRAINT "fk_Dept_Employees_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
-
 ALTER TABLE "Dept_Employees" ADD CONSTRAINT "fk_Dept_Employees_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Employees" ADD CONSTRAINT "fk_Dept_Employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
 
 ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "Departments" ("dept_no");
@@ -74,6 +68,6 @@ REFERENCES "Employees" ("emp_no");
 ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "Titles" ("title_id");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp-no" FOREIGN KEY("emp-no")
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
 
