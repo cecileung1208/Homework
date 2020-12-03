2efeb4b753c6e6814f2023606ede8084304f2cea
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2020-12-02 20:03:25 -0500

    update

--- Unit 10 - SQL_Alchemy Challenge/app.py
+++ Unit 10 - SQL_Alchemy Challenge/app.py
@@ -23,12 +23,61 @@ app = Flask(__name__)
 #Define main route
 @app.route("/")
 def welcome():
-    return "Welcome to my Weather Alchemy Page"
-
+    return "Welcome to Hawaii's Alchemy Page!!<br>"
+        
 #Define precipitation route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    return "This is the precipitation for the last year"
+    session = Session(engine)
+    last_date= session.query(func.max(Measurement.date)).first()
+    latest_date =  list(np.ravel(last_date))
+    year = int(latest_date[0][0]+latest_date[0][1]+latest_date[0][2]+latest_date[0][3])
+    month =int(latest_date[0][5]+latest_date[0][6])
+    date = int(latest_date[0][8]+latest_date[0][9])
+    year_ago = dt.date(year, month, date) - dt.timedelta(days=365)
+    year_later = dt.date(year,month, date)
+    one_year_prcp = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > year_ago, Measurement.date<= year_later).all()
+    session.close()
+    return jsonify(one_year_prcp)
+
+#Define stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    stations= session.query(Station.station, Station.name).all()
+    session.close()
+    return jsonify(stations)
+
+#Define tobs route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    last_date= session.query(func.max(Measurement.date)).first()
+    latest_date =  list(np.ravel(last_date))
+    year = int(latest_date[0][0]+latest_date[0][1]+latest_date[0][2]+latest_date[0][3])
+    month =int(latest_date[0][5]+latest_date[0][6])
+    date = int(latest_date[0][8]+latest_date[0][9])
+    year_ago = dt.date(year, month, date) - dt.timedelta(days=365)
+    year_later = dt.date(year,month, date)
+    most_active_yearly_temp = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > year_ago, Measurement.date<= year_later).filter(Measurement.station == 'USC00519281').all()
+    session.close()
+    return jsonify(most_active_yearly_temp)
+
+@app.route("/api/v1.0/tobs_test")
+def tobs_test():
+    session = Session(engine)
+    results = session.query(Station.name, Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= "2016-08-24", Measurement.date <= "2017-08-23").\
+        all()
+    tobs_list = []
+    for result in results:
+        row = {}
+        row["Station"] = result[0]
+        row["Date"] = result[1]
+        row["Temp"] = int(result[2])
+        tobs_list.append(row)
+    session.close()
+    return jsonify(tobs_list)
 
 #Define main behavior
 if __name__ == "__main__":
