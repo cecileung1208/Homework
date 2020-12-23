b431b9daf97636ab1d80e409b70a9923742951e1
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2020-12-23 14:29:40 -0500

    update index.html & app.py

--- Unit 12 - Web Scraping Challenge/Mission to Mars/app.py
+++ Unit 12 - Web Scraping Challenge/Mission to Mars/app.py
@@ -9,29 +9,29 @@ app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+    
+# create route that renders index.html template
 
 @app.route("/")
 def home():
 
     # Find one record of data from the mongo database
-    mars = mongo.db.collection.find_one()
+    mars = mongo.db.mars_data.find_one()
 
     # Return template and data
     return render_template("index.html", mars_data = mars)
 
-    
-# create route that renders index.html template
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape fuction
-    mars_data = scrape_mars.scrape_info()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.mars_data.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    return "Scraping Complete!!"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Unit 12 - Web Scraping Challenge/Mission to Mars/scrape_mars.py
+++ Unit 12 - Web Scraping Challenge/Mission to Mars/scrape_mars.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Mission to Mars
-
-# ### Import Dependencies
-
-
 # Import dependencies
 from bs4 import BeautifulSoup
 import requests
@@ -13,20 +5,19 @@ import pandas as pd
 import pymongo
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
-import time
-
-
-# ### NASA Mars News
 
 # Use Splinter to Run URL to extract news headlines and paragraph text
 def init_browser():
-    executable_path = {'executable_path': 'chromedriver'} #ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
-    return browser
+    executable_path = {'executable_path': 'chromedriver'} 
+    return Browser('chrome', **executable_path, headless=False)
+
+### NASA Mars News
 
 
-def scrape_info():
+def scrape():
+    
     browser = init_browser()
+
     # URL of page to be scraped
     url = 'https://mars.nasa.gov/news/'
 
@@ -41,21 +32,17 @@ def scrape_info():
 
     # Get html code of latest news
     results = soup.find('li', class_='slide')
-
-
+ 
     # Extract the latest news and its paragraph text
     news_title = results.find('div', class_='content_title').text
     news_text = results.find('div', class_='article_teaser_body').text
 
-
-    time.sleep(2)
-
     ### JPL Mars Space Images - Featured Image
     # URL to visit
-    jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 
     # Visit through chrome browser 
-    browser.visit(jpl_url)
+    browser.visit(url)
 
     # Create html to extract news information
     html_jpl = browser.html
@@ -63,43 +50,35 @@ def scrape_info():
     # Create BeautifulSoup object; parse with 'html.parser'
     soup_jpl = BeautifulSoup(html_jpl,'html.parser')
 
+    # Get html code to find the featured image link
+    print(soup_jpl.prettify())
 
     # Extract the link for the featured image
     image = soup_jpl.find('div', class_='carousel_items').a['data-fancybox-href']
-
-
+ 
     #Link main URL to image link to get the full URL of the image in JPL website
     main_url = 'https://www.jpl.nasa.gov'
     featured_image_url= main_url + image
 
-
-    time.sleep(2)
-
     ### Mars Facts
-
-    #Mars Facts URL
+    # Mars Facts URL
     url_mars_facts= 'https://space-facts.com/mars/'
-
 
     # Use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
     table_mars= pd.read_html(url_mars_facts)
 
-
     # The above output parses all tables from the URL.  We only need the first table.
     mars_df = table_mars[0]
-
 
     #Format the table by changing column names
     column_names = {mars_df.columns[0]: 'Description', mars_df.columns[1]: 'Value'}
     mars_df.rename(columns = column_names)
 
-
     # Use Pandas to convert the data to a HTML table string.
     mars_facts_html = mars_df.to_html()
-    time.sleep(2)
+    print(mars_facts_html)
 
-    # Mars Hemisphere
-
+    ### Mars Hemisphere
     # URL of page to be scraped
     url_mars_hemisphere = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
@@ -109,46 +88,33 @@ def scrape_info():
     # create html to extract news information
     html_astropedia = browser.html
 
-
     # Create BeautifulSoup object; parse with 'html.parser'
     soup_mars_hemisphere = BeautifulSoup(html_astropedia,'html.parser')
-
 
     hemi_img_urls = []
 
     h3links= browser.find_by_tag("h3")
 
-
     for items in range(len(h3links)):
         hemidict = {}
-    
+        
         browser.find_by_css('h3')[items].click()
-    
+        
         ## titles
         hemidict['title']=browser.find_by_css('h2.title').text
-    
+        
         ## images
         image_url= browser.links.find_by_text('Sample').first
-        hemidict['img_url'] = image_url['href']
+        hemidict['image_url'] = image_url['href']
         hemi_img_urls.append(hemidict)
         browser.back()
-    
-    time.sleep(2)
-
-    # create dictionary to hold all mars info scraped
-
+   
     mars_data = {
-            "news_title": news_title,
-            "news_text": news_text,
-            "featured_image_url": featured_image_url,
-            "html_table": mars_facts_html,
-            "hemisphere_img_urls": hemi_img_urls
-    }
+                "news_title": news_title,
+                "news_text": news_text,
+                "featured_image_url": featured_image_url,
+                "html_table": mars_facts_html,
+                "hemisphere_img_urls": hemi_img_urls
+        }
 
-
-
-  # Close the browser after scraping
-    browser.quit()
-
-    # Return results
     return mars_data
